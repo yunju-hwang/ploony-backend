1600a927d259ccd2aeb780a7cf4d9c8ead33dca6
yunju <duswldbswn@pusan.ac.kr>
2024-09-15 16:33:27 +0900

    api composition

---
+++ app.js
@@ -0,0 +1,68 @@
+import express from "express";
+import plants from "./data/mock.js";
+
+const app = express();
+app.use(express.json());
+
+//식물 전체 리스트 조회
+app.get("/plants", (req, res) => {
+  if (plants) {
+    res.send(plants);
+  } else {
+    res.status(404).send({ message: "Cannot find plants information" });
+  }
+});
+
+//식물 상세 정보 조회
+app.get("/plants/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const plant = plants.find((plant) => plant.id === id);
+  if (plant) {
+    res.send(plant);
+  } else {
+    res.status(404).send({ message: "Cannot find given id" });
+  }
+});
+
+//식물 생성
+app.post("/plants", (req, res) => {
+  const newPlant = req.body;
+  const ids = plants.map((plant) => plant.id);
+  newPlant.id = Math.max(...ids) + 1;
+
+  newPlant.temperature = null;
+  newPlant.humidity = null;
+  newPlant.light = null;
+  plants.push(newPlant);
+  res.status(201).send(newPlant);
+});
+
+//사용자 식물 정보 수정
+app.patch("/plants/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const plant = plants.find((plant) => plant.id === id);
+  if (plant) {
+    Object.keys(req.body).forEach((key) => {
+      plant[key] = req.body[key];
+    });
+    res.send(plant);
+  } else {
+    res.status(404).send({ message: "Cannot find given id" });
+  }
+});
+
+//아두이노로부터 식물 정보 업데이트  (나중에 수정 필요)
+app.patch("/plants/:id/sensor-data", (req, res) => {
+  const id = Number(req.params.id);
+  const plant = plants.find((plant) => plant.id === id);
+  if (plant) {
+    Object.keys(req.body).forEach((key) => {
+      plant[key] = req.body[key];
+    });
+    res.send(plant);
+  } else {
+    res.status(404).send({ message: "Cannot find given id" });
+  }
+});
+
+app.listen(3000, () => console.log("Server Started"));
---
+++ data/mock.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    id: 1,
+    name: "Aloe Vera",
+    species: "Aloe",
+    startDate: "2023-01-15T00:00:00.000Z",
+    photoUrl: "http://example.com/aloe-vera.jpg",
+    temperature: 25.2,
+    humidity: 45,
+    light: 500,
+  },
+
+  {
+    id: 2,
+    name: "Test",
+    species: "test species",
+    startDate: "2023-01-15T00:00:00.000Z",
+    photoUrl: "http://example.com/aloe-vera.jpg",
+    temperature: null,
+    humidity: null,
+    light: null,
+  },
+];
+
+export default data;
