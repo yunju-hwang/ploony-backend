525a8c710c678523304510c47520588cea2ba5b8
yunju <duswldbswn@pusan.ac.kr>
2024-10-08 10:18:12 +0900

    CHAT GPT 답변 업데이트

--- openai.js
+++ openai.js
@@ -8,14 +8,14 @@ const openai = new OpenAI({
 
 export async function analyzePlantData(plant) {
   try {
-    const prompt = `식물데이터 분석:\nName: ${plant.name}\nSpecies: ${plant.species}\nTemperature: ${plant.temperature}\nHumidity: ${plant.humidity}\nLight: ${plant.light}, 한국어로 대답하고 간단하게 90자 내로 문장이 완벽하게 끝나도록 작성해줘`;
+    const prompt = `식물데이터 분석:\nName: ${plant.name}\nSpecies: ${plant.species}\nTemperature: ${plant.temperature}\nHumidity: ${plant.humidity}\nLight: ${plant.light}, 한국어로 대답하고 간단하게 80자 내로 문장이 완벽하게 끝나도록 작성해줘`;
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
         { role: "user", content: prompt }, // 사용자 메시지를 messages 배열에 추가
       ],
-      max_tokens: 150,
+      max_tokens: 200,
     });
 
     const answer = response.choices[0].message.content;
